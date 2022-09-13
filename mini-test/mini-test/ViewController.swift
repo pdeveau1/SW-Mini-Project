47853be9c25ddff7c446c790f4c69841635e1cd4
OwenZhang001122 <98358877+OwenZhang001122@users.noreply.github.com>
2022-09-13 16:29:10 -0400

    Add files via upload

---
+++ mini-test/mini-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mini-test
+//
+//  Created by Shun Zhang  on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
