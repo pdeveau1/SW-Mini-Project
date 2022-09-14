4cf4c1de42327b1c6dce5f1bea12797bfd4e9246
pdeveau1 <pdeveau@bu.edu>
2022-09-14 13:05:39 -0400

    write a class Bot that uses botometer to check the results of the account and gets the score of if it is a bot or not, write script to test class

---
+++ Twitter/bot_script.py
@@ -0,0 +1,5 @@
+import check_bot
+
+bot = check_bot.Bot('@clayadavis')
+print(bot.get_account())
+print(bot.get_score())
--- Twitter/check_bot.py
+++ Twitter/check_bot.py
@@ -1,9 +1,44 @@
 from decouple import config
+from botometer import Botometer
 
-rapidapi_key = config('rapidapi_key',default='')
-twitter_app_auth = {
-            'consumer_key': config('consumer_key',default=""),
-            'consumer_secret': config('consumer_secret',default="")
-            }
+class Bot:
+    def __init__(self,account):
+        self.account = account
+        self.result = {}
+        self.score = {}
 
-print(rapidapi_key)
+    def get_account(self):
+        rapidapi_key = config('rapidapi_key',default='')
+        twitter_app_auth = {
+                    'consumer_key': config('consumer_key',default=""),
+                    'consumer_secret': config('consumer_secret',default=""),
+                    'access_token': config('access_token',default=""),
+                    'access_token_secret': config('access_token_secret',default="")
+                    }
+
+        botometer = Botometer(wait_on_ratelimit=True,
+                                rapidapi_key=rapidapi_key,
+                                **twitter_app_auth)
+
+        self.result = botometer.check_account(self.account)
+        return(self.result)
+    """
+    def check_accounts(self):
+        rapidapi_key = config('rapidapi_key',default='')
+        twitter_app_auth = {
+                    'consumer_key': config('consumer_key',default=""),
+                    'consumer_secret': config('consumer_secret',default=""),
+                    'access_token': config('access_token',default=""),
+                    'access_token_secret': config('access_token_secret',default="")
+                    }
+
+        botometer = Botometer(wait_on_ratelimit=True,
+                                rapidapi_key=rapidapi_key,
+                                **twitter_app_auth)
+
+        for screen_name, result in bom.check_accounts_in(self.accounts):
+            self.results[screen_name] = botometer.check_account(screen_name)
+    """
+    def get_score(self):
+        self.score = self.result['cap']
+        return(self.score)
