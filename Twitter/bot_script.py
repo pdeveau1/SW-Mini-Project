a683dd79ba96f4e07db025c168ae1dfca0752452
pdeveau1 <pdeveau@bu.edu>
2022-09-14 13:15:45 -0400

    Add function to Bot class that will check if user is bot based of the majority language score and add that to test script

--- Twitter/bot_script.py
+++ Twitter/bot_script.py
@@ -3,3 +3,4 @@ import check_bot
 bot = check_bot.Bot('@clayadavis')
 print(bot.get_account())
 print(bot.get_score())
+print(bot.get_bot())
--- Twitter/check_bot.py
+++ Twitter/check_bot.py
@@ -6,7 +6,9 @@ class Bot:
         self.account = account
         self.result = {}
         self.score = {}
+        self.is_bot = False
 
+    #Get the account information of the user
     def get_account(self):
         rapidapi_key = config('rapidapi_key',default='')
         twitter_app_auth = {
@@ -39,6 +41,24 @@ class Bot:
         for screen_name, result in bom.check_accounts_in(self.accounts):
             self.results[screen_name] = botometer.check_account(screen_name)
     """
+
+    #Get the english and universal bot score of the user
     def get_score(self):
         self.score = self.result['cap']
         return(self.score)
+    
+    #Check if the bot is a user based off of the majority language score
+    def get_bot(self):
+        #get majority language to determine what score to use for bot check
+        if self.result['user']['majority_lang'] == 'en':
+            score = self.score['english']
+        else:
+            score = self.score['universal']
+
+        #Assume score of more than 0.6 is an indication that an account is a bot
+        if score > 0.6:
+            self.is_bot = True
+        else:
+            self.is_bot = False
+
+        return self.is_bot
