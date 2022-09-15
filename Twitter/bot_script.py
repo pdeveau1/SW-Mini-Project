2f2daddc72aec1922795193ba520785ab25fa7f2
pdeveau1 <pdeveau@bu.edu>
2022-09-15 16:27:11 -0400

    Use 'https://cloud.google.com/natural-language/docs/classifying-text' as reference to classify the content of a tweet

--- Twitter/bot_script.py
+++ Twitter/bot_script.py
@@ -8,3 +8,4 @@ print(bot.calc_sentiment())
 """
 bot = user.User('@hey')
 bot.sample_analyze_entities('California is a state.')
+bot.sample_classify_text('That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows.')
--- Twitter/user.py
+++ Twitter/user.py
@@ -219,3 +219,36 @@ class User:
         # the language specified in the request or, if not specified,
         # the automatically-detected language.
         print(u"Language of the text: {}".format(response.language))
+
+    def sample_classify_text(self,text_content):
+        """
+        Classifying Content in a String
+
+        Args:
+        text_content The text content to analyze. Must include at least 20 words.
+        """
+
+        client = language_v1.LanguageServiceClient()
+
+        # text_content = 'That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows.'
+
+        # Available types: PLAIN_TEXT, HTML
+        type_ = language_v1.Document.Type.PLAIN_TEXT
+
+        # Optional. If not specified, the language is automatically detected.
+        # For list of supported languages:
+        # https://cloud.google.com/natural-language/docs/languages
+        language = "en"
+        document = {"content": text_content, "type_": type_, "language": language}
+
+        response = client.classify_text(request = {'document': document})
+        # Loop through classified categories returned from the API
+        for category in response.categories:
+            # Get the name of the category representing the document.
+            # See the predefined taxonomy of categories:
+            # https://cloud.google.com/natural-language/docs/categories
+            print(u"Category name: {}".format(category.name))
+            # Get the confidence. Number representing how certain the classifier
+            # is that this category represents the provided text.
+            print(u"Confidence: {}".format(category.confidence))
+
