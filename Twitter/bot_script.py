6720b6909360c5586a8eba7f2ad9de613b6521c7
pdeveau1 <pdeveau@bu.edu>
2022-09-15 16:25:05 -0400

    Use 'https://cloud.google.com/natural-language/docs/analyzing-entities' as reference to get entity of string

--- Twitter/bot_script.py
+++ Twitter/bot_script.py
@@ -1,7 +1,10 @@
 import json
 import user
-
+"""
 username = input('Enter a username to check: ')
 bot = user.User('@' + username)
 print(bot.get_bot())
 print(bot.calc_sentiment())
+"""
+bot = user.User('@hey')
+bot.sample_analyze_entities('California is a state.')
