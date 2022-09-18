26b3b7e2be14b9e3811363ae6bed43bb9f9113a6
pdeveau1 <pdeveau@bu.edu>
2022-09-18 13:08:11 -0400

    add view

--- twitter_project/twitter_api/views.py
+++ twitter_project/twitter_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .serializers import TwitterUserSerializer
+from .models import Twitter_User
+
+class TwitterUserViewSet(viewsets.ModelViewSet):
+    queryset = Twitter_User.objects.all().order_by('username')
+    serializer_class = TwitterUserSerializer
